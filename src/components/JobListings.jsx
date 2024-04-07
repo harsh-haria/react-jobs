f1e15feecbae751fb309e97dd5520adba52ffe1c
Harsh Haria <harshharia1234@gmail.com>
2024-04-07 19:55:28 +0530

    added useEffect to query data from the database

--- src/components/JobListings.jsx
+++ src/components/JobListings.jsx
@@ -1,8 +1,28 @@
+import { useEffect, useState } from "react";
+
 import jobs from "../jobs.json";
 import JobListing from "./JobListing";
 
 const JobListings = ({ isHome = false }) => {
-	const paginatedArray = isHome ? jobs.slice(0, 3) : jobs;
+	const [jobs, setJobs] = useState([]);
+	const [loading, setLoading] = useState(true);
+
+	useEffect(() => {
+		const fetchJobs = async () => {
+			try {
+				let response = await fetch("http://localhost:8000/jobs");
+				let data = await response.json();
+				setJobs(data);
+			} catch (error) {
+				console.log(error);
+			} finally {
+				setLoading(false);
+			}
+		};
+		fetchJobs();
+	}, []);
+
+	// const paginatedArray = isHome ? jobs.slice(0, 3) : jobs;
 	return (
 		<section className="bg-blue-50 px-4 py-10">
 			<div className="container-xl lg:container m-auto">
@@ -10,7 +30,7 @@ const JobListings = ({ isHome = false }) => {
 					{isHome ? `Recent Jobs` : `Browse Jobs`}
 				</h2>
 				<div className="grid grid-cols-1 md:grid-cols-3 gap-6">
-					{paginatedArray.map((jobItem) => (
+					{jobs.map((jobItem) => (
 						<JobListing key={jobItem.id} job={jobItem} />
 					))}
 				</div>
