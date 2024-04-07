8d4d072f5a632133a307fb345286d101c1ed65f6
Harsh Haria <harshharia1234@gmail.com>
2024-04-07 18:17:29 +0530

    Added mutliple things in the project
    1) Added 404 page not found page
    2) Added Link to in place of a href
    3) change the structure of the project, Added routes in the app.jsx file
       for clean code.

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,7 +1,17 @@
 import Hero from "../components/Hero";
+import HomeCards from "../components/HomeCards";
+import JobListings from "../components/JobListings";
+import ViewAllJobs from "../components/ViewAllJobs";
 
 const HomePage = () => {
-	return <Hero />;
+	return (
+		<>
+			<Hero />
+			<HomeCards />
+			<JobListings />
+			<ViewAllJobs />
+		</>
+	);
 };
 
 export default HomePage;
