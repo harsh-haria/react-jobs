dfe06c357dcf25019df54e91eeb54e3115a4e923
Harsh Haria <harshharia1234@gmail.com>
2024-04-10 01:40:13 +0530

    completed the project. added multiple components
    1) Page to add a job completed.
    2) Page to edit a job completed.
    3) Routes established for point 1&2.
    4) toasts added for success and errors of the add edit operations
    5) Delete job functionalit added as well.

--- src/App.jsx
+++ src/App.jsx
@@ -12,19 +12,65 @@ import MainLayout from "./layouts/MainLayout";
 import JobsPage from "./pages/JobsPage";
 import NotFoundPage from "./pages/NotFoundPage";
 import JobPage, { jobLoader } from "./pages/JobPage";
-
-const router = createBrowserRouter(
-	createRoutesFromElements(
-		<Route path="/" element={<MainLayout />}>
-			<Route index element={<HomePage />} />
-			<Route path="/jobs" element={<JobsPage />} />
-			<Route path="/jobs/:id" element={<JobPage />} loader={jobLoader} />
-			<Route path="*" element={<NotFoundPage />} />
-		</Route>
-	)
-);
+import AddJobPage from "./pages/AddJobPage";
+import EditJobPage from "./pages/EditJobPage";
 
 const App = () => {
+	const addJobSubmit = async (newJob) => {
+		// console.log(newJob);
+		const newJobSave = await fetch("/api/jobs", {
+			method: "POST",
+			headers: {
+				"Content-Type": "application/json",
+			},
+			body: JSON.stringify(newJob),
+		});
+		return;
+	};
+
+	const deleteJob = async (id) => {
+		const deleteJobSave = await fetch(`/api/jobs/${id}`, {
+			method: "DELETE",
+		});
+		// console.log(id);
+		return;
+	};
+
+	const updateJob = async (updatedJob) => {
+		const updateJobSave = await fetch(`/api/jobs/${updatedJob.id}`, {
+			method: "PUT",
+			headers: {
+				"Content-Type": "application/json",
+			},
+			body: JSON.stringify(updatedJob),
+		});
+		return;
+	};
+
+	const router = createBrowserRouter(
+		createRoutesFromElements(
+			<Route path="/" element={<MainLayout />}>
+				<Route index element={<HomePage />} />
+				<Route path="/jobs" element={<JobsPage />} />
+				<Route
+					path="/jobs/:id"
+					element={<JobPage deleteJob={deleteJob} />}
+					loader={jobLoader}
+				/>
+				<Route
+					path="/edit-job/:id"
+					element={<EditJobPage updateJob={updateJob} />}
+					loader={jobLoader}
+				/>
+				<Route
+					path="/add-job"
+					element={<AddJobPage addJobSubmit={addJobSubmit} />}
+				/>
+				<Route path="*" element={<NotFoundPage />} />
+			</Route>
+		)
+	);
+
 	return <RouterProvider router={router} />;
 };
 
--- src/pages/JobPage.jsx
+++ src/pages/JobPage.jsx
@@ -1,10 +1,29 @@
-import { useParams, useLoaderData } from "react-router-dom";
+import { useParams, useLoaderData, useNavigate } from "react-router-dom";
 import { FaArrowLeft, FaMapMarked } from "react-icons/fa";
 import { Link } from "react-router-dom";
+import { toast } from "react-toastify";
 
-const JobPage = () => {
+const JobPage = ({ deleteJob }) => {
+	const navigate = useNavigate();
 	const { id } = useParams();
 	const job = useLoaderData();
+
+	const onDeleteClick = (JobId) => {
+		const confirmDelete = window.confirm(
+			"Are you sure you want to delete this job ?"
+		);
+
+		if (!confirmDelete) {
+			return;
+		}
+
+		deleteJob(JobId);
+
+		toast.success("Job deleted.");
+
+		navigate("/jobs");
+	};
+
 	return (
 		<>
 			{/* <!-- Go Back --> */}
@@ -76,12 +95,15 @@ const JobPage = () => {
 							<div className="bg-white p-6 rounded-lg shadow-md mt-6">
 								<h3 className="text-xl font-bold mb-6">Manage Job</h3>
 								<Link
-									to={`/jobs/edit/${job.id}`}
+									to={`/edit-job/${job.id}`}
 									className="bg-indigo-500 hover:bg-indigo-600 text-white text-center font-bold py-2 px-4 rounded-full w-full focus:outline-none focus:shadow-outline mt-4 block"
 								>
 									Edit Job
 								</Link>
-								<button className="bg-red-500 hover:bg-red-600 text-white font-bold py-2 px-4 rounded-full w-full focus:outline-none focus:shadow-outline mt-4 block">
+								<button
+									onClick={() => onDeleteClick(job.id)}
+									className="bg-red-500 hover:bg-red-600 text-white font-bold py-2 px-4 rounded-full w-full focus:outline-none focus:shadow-outline mt-4 block"
+								>
 									Delete Job
 								</button>
 							</div>
